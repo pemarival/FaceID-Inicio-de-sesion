65172d64d9eae0110bed51655b4bb3e0b2da2818
Pemarival <valenzuelapenagosm@gmail.com>
2026-03-26 17:33:20 -0500

    Estructura inicial

---
+++ login-app/js/main.js
@@ -0,0 +1,14 @@
+const password = document.getElementById("password");
+const togglePassword = document.getElementById("togglePassword");
+
+togglePassword.addEventListener("mousedown", () => {
+    password.type = "text";
+});
+
+togglePassword.addEventListener("mouseup", () => {
+    password.type = "password";
+});
+
+togglePassword.addEventListener("mouseleave", () => {
+    password.type = "password";
+});
